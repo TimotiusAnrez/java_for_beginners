0ce7f9501c026cc0dae87b1cdd7901433bf2c182
Timotius Anrez <71978054+TimotiusAnrez@users.noreply.github.com>
2022-12-14 14:14:25 +0700

    assignment done

--- assigment_10/app/src/App.java
+++ assigment_10/app/src/App.java
@@ -1,5 +1,34 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Integer[] arr = {15,-22,123,47,81,-12,-29};
+        int tmp;
+
+        //buble sort ascending order
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[i] > arr[j]){
+                    tmp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = tmp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+        //buble sort descending order
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[i] < arr[j]){
+                    tmp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = tmp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+        
     }
 }
